caa2b02feafac636ba0cadcb28208d966752d003
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-05-06 12:58:56 +0530

    initilize method

---
+++ src/Stack.java
@@ -0,0 +1,30 @@
+public class Stack {
+
+    public void push(int n){
+
+    }
+
+    public void pop(){
+
+    }
+
+    public boolean empty(){
+        return false;
+    }
+
+    public int peek(){
+        return 0;
+    }
+
+    public void clear(){
+
+    }
+
+    public void print(){
+
+    }
+
+    public boolean contains(int n){
+        return true;
+    }
+}
